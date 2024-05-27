f7cf4071031a7fb6a47f575aaf60f2e7b191d8ba
kelvin <kelvinmutugi336@gmil.com>
2024-05-27 14:24:35 +0300

    solved lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+function writeCards(names, eventName) {
+  let thankYouMessages = [];
+
+  if (!Array.isArray(names)) {
+    return "Please provide an array of names.";
+  }
+
+  names.forEach((name) => {
+    let message = `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+    thankYouMessages.push(message);
+  });
+
+  return thankYouMessages;
+}
+function countDown(int) {
+  while (int >= 0) {
+    console.log(int);
+    int--;
+  }
+  return int;
+}
+
+const nums = countDown(10);
+console.log(nums);
